0a18396b2aff9190287c9947697e2797d48074cc
CHELLAMUTHUMARI P <chellamuthup16072002@gmail.com>
2025-09-07 11:56:03 +0530

    Update Live Server

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext(null);
 
 export const UserContextProvider = ({children}) => {
-    const url = 'http://localhost:5000/api';
+    const url = 'https://simple-auth-system.onrender.com/api';
 
 
     const contextValue = {
